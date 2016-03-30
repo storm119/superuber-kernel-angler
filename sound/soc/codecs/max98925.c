d3b0ad3afa96f191407075e2ce039516c63eaa62
Forrest Marble <forrestmarble@gmail.com>
2016-03-30 15:10:00 -0400

    Sound Control: add Francos sound control with high performance audio control pull from here: https://github.com/Elite-Kernels/Elite_M8/commit/b3c14f0f5d8f5d5f847431e489fd16ccd6f5a733
    headphone and microphone gain control from @franciscofranco github here: https://github.com/franciscofranco/angler

--- sound/soc/codecs/max98925.c
+++ sound/soc/codecs/max98925.c
@@ -45,6 +45,10 @@ static uint8_t *payload = (uint8_t *)&param_array[100];
 
 static DEFINE_MUTEX(dsm_lock);
 
+#define DEFAULT_SPEAKER_GAIN 20
+int speaker_l_gain = DEFAULT_SPEAKER_GAIN;
+int speaker_r_gain = DEFAULT_SPEAKER_GAIN;
+
 static struct reg_default max98925_reg[] = {
 	{ 0x00, 0x00 }, /* Battery Voltage Data */
 	{ 0x01, 0x00 }, /* Boost Voltage Data */
@@ -374,6 +378,16 @@ static const unsigned int max98925_spk_tlv[] = {
 	1, 31, TLV_DB_SCALE_ITEM(-600, 100, 0),
 };
 
+void update_speakers_l_gain(int vol_boost)
+{
+	speaker_l_gain = DEFAULT_SPEAKER_GAIN + vol_boost;
+}
+
+void update_speakers_r_gain(int vol_boost)
+{
+	speaker_r_gain = DEFAULT_SPEAKER_GAIN + vol_boost;
+}
+
 static int max98925_left_gain_get(struct snd_kcontrol *kcontrol,
 				struct snd_ctl_elem_value *ucontrol)
 {
@@ -397,6 +411,9 @@ static int max98925_left_gain_put(struct snd_kcontrol *kcontrol,
 	struct snd_soc_codec *codec = snd_kcontrol_chip(kcontrol);
 	struct max98925_priv *max98925 = snd_soc_codec_get_drvdata(codec);
 	unsigned int sel = ucontrol->value.integer.value[0];
+
+	if (sel && speaker_l_gain != DEFAULT_SPEAKER_GAIN)
+		sel = speaker_l_gain;
 
 	if (sel < ((1 << M98925_SPK_GAIN_WIDTH) - 1)) {
 		regmap_update_bits(max98925->regmapL, MAX98925_R02D_GAIN,
@@ -436,6 +453,9 @@ static int max98925_right_gain_put(struct snd_kcontrol *kcontrol,
 	struct snd_soc_codec *codec = snd_kcontrol_chip(kcontrol);
 	struct max98925_priv *max98925 = snd_soc_codec_get_drvdata(codec);
 	unsigned int sel = ucontrol->value.integer.value[0];
+
+	if (sel && speaker_r_gain != DEFAULT_SPEAKER_GAIN)
+                sel = speaker_r_gain;
 
 	if (sel < ((1 << M98925_SPK_GAIN_WIDTH) - 1)) {
 		regmap_update_bits(max98925->regmapR, MAX98925_R02D_GAIN,
@@ -483,7 +503,7 @@ static int max98925_left_en_put(struct snd_kcontrol *kcontrol,
 			pr_err("%s: modify spk&recv to high failed nret = %d\n",
 				__func__, nret);
 	} else {
-		pr_err("%s: spk&rcver switch gpio had pulled down", __func__);
+		pr_debug("%s: spk&rcver switch gpio had pulled down", __func__);
 	}
 
 	max98925->left_en = sel << M98925_EN_SHIFT;

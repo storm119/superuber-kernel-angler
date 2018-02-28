f21c35d2ecb3596f5a3a0977acbc6294b4ecb89b
yarost12 <yaro330@gmail.com>
2018-02-28 22:27:15 +0300

    adreno_tz: add 'adreno_debug' for logging freq changes printk calls were too long to cut them, throw them into a dedicated function. Why not? Signed-off-by: yarost12 <yaro330@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -322,6 +322,8 @@ extern int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq
 #endif
 
 #if 1
+static int adrenoboost_debug(struct devfreq *devfreq, unsigned long *freq,
+				u32 *flag, int jump_dir);
 /*
 * mapping gpu level calculated linear conservation half curve
 * values into a bell curve of conservation (lower is higher freq level)
@@ -498,6 +500,7 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 		level += val;
 		level = max(level, 0);
 		level = min_t(int, level, devfreq->profile->max_state - 1);
+		adrenoboost_debug(devfreq, freq, flag, 0);
 		priv->bin.last_level = level;
 	} else {
 		if (val) {
@@ -510,6 +513,7 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 			*/
 			if (val<0 && priv->bin.cycles_keeping_level <
 				conservation_map_up[ last_level ]) {
+				adrenoboost_debug(devfreq, freq, flag, 1);
 			} else
 			/*
 			* going downwards in frequency let it happen hard in
@@ -517,6 +521,7 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 			*/
 			if (val>0 && priv->bin.cycles_keeping_level <
 				conservation_map_down[ last_level ])  {
+				adrenoboost_debug(devfreq, freq, flag, 2);
 			} else
 			{
 				level += val;
@@ -526,6 +531,7 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 				priv->bin.cycles_keeping_level = 0;
 				// set new last level
 				priv->bin.last_level = level;
+				adrenoboost_debug(devfreq, freq, flag, 0);
 			}
 		}
 	}
@@ -542,6 +548,28 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 	*freq = devfreq->profile->freq_table[level];
 	return 0;
 }
+
+#if 1
+static int adrenoboost_debug(struct devfreq *devfreq, unsigned long *freq,
+				u32 *flag, int jump_dir)
+{
+	struct devfreq_msm_adreno_tz_data *priv = devfreq->data;
+	struct devfreq_dev_status stats;
+	int val, level = 0;
+	int last_level = priv->bin.last_level;
+	if (!jump_dir)
+		pr_info("ADRENO jumping\n");
+	else if (jump_dir == 1)
+		pr_info("ADRENO NOT jumping UP\n");
+	else
+		pr_info("ADRENO NOT jumping DOWN\n");
+	pr_info("level = %d last_level = %d total=%d busy=%d original busy_time=%d\n",
+			level, priv->bin.last_level,
+			(int)priv->bin.total_time,
+			(int)priv->bin.busy_time, (int)stats.busy_time);
+	return 0;
+}
+#endif
 
 static int tz_notify(struct notifier_block *nb, unsigned long type, void *devp)
 {

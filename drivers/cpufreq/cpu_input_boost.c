b33ed607c766b9d03c15959f2196e5621acf4cce
yarost12 <yaro330@gmail.com>
2018-03-08 02:24:13 +0300

    cpu_input_boost: prevent init from fucking everything up Store policy->min after init is done. Signed-off-by: yarost12 <yaro330@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -103,6 +103,12 @@ static bool validate_cpu_freq(unsigned int cpu, uint32_t *freq);
 
 static struct notifier_block notif;
 
+static bool validate_cpu_freq(struct cpufreq_frequency_table *pos,
+		uint32_t *freq);
+static bool is_initd(const char* p)
+{
+	return strncmp(p, "init", sizeof("init"));
+}
 static void ib_boost_main(struct work_struct *work)
 {
 	struct boost_policy *b = boost_policy_g;
@@ -238,11 +244,16 @@ static int do_cpu_boost(struct notifier_block *nb,
 	struct ib_config *ib = &b->ib;
 	uint32_t boost_freq, state;
 	unsigned int min_freq_boosted;
+	bool initd = !is_initd(current->comm);
 	bool ret;
 
 	if (action != CPUFREQ_ADJUST)
 		return NOTIFY_OK;
 
+	if (initd) {
+		pr_err("I'm eating, fuck off!");
+		return NOTIFY_OK;
+	}
 	state = get_boost_state(b);
 
 	/*

MotorolaMobilityLLC/kernel-msm

msm: camera: sensor: Disable debug logs
Disable debug logs that are not important every time.

Change-Id: I31fb558013c661cf6978ba06601ae37b294b0d9f
Signed-off-by: Rajesh Bondugula <rajeshb@codeaurora.org>

drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
@@ -1200,7 +1200,7 @@ static int32_t msm_sensor_driver_parse(struct msm_sensor_ctrl_t *s_ctrl)
 
 	/* Store sensor control structure in static database */
 	g_sctrl[s_ctrl->id] = s_ctrl;
-	pr_err("g_sctrl[%d] %p", s_ctrl->id, g_sctrl[s_ctrl->id]);
+	CDBG("g_sctrl[%d] %p", s_ctrl->id, g_sctrl[s_ctrl->id]);
 
 	return rc;
 

storm119/superuber-kernel-angler

Signed-off-by: Joe Maples <joe@frap129.org>

drivers/media/platform/msm/camera_v2/sensor/msm_sensor_driver.c
@@ -1183,7 +1183,7 @@ static int32_t msm_sensor_driver_parse(struct msm_sensor_ctrl_t *s_ctrl)
 
 	/* Store sensor control structure in static database */
 	g_sctrl[s_ctrl->id] = s_ctrl;
-	pr_err("g_sctrl[%d] %pK", s_ctrl->id, g_sctrl[s_ctrl->id]);
+	CDBG("g_sctrl[%d] %pK", s_ctrl->id, g_sctrl[s_ctrl->id]);
 
 	return rc;
 
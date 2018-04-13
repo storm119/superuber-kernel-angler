60066dc15a55132f0e25df1b10dc50722962032d
storm119 <storm119@gmail.com>
2018-04-13 06:23:58 +0200

    cpufreq: add description to "cpufreq_notify_utilization"  * no codes change tho ;)
    Signed-off-by: Robby Duran <storm119@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -347,6 +347,13 @@ void cpufreq_notify_transition(struct cpufreq_policy *policy,
 }
 EXPORT_SYMBOL_GPL(cpufreq_notify_transition);
 
+/**
+ * cpufreq_notify_utilization - notify CPU userspace about CPU utilization
+ * change
+ *
+ * This function is called everytime the CPU load is evaluated by the
+ * ondemand governor. It notifies userspace of cpu load changes via sysfs.
+ */
 void cpufreq_notify_utilization(struct cpufreq_policy *policy,
 		unsigned int util)
 {
